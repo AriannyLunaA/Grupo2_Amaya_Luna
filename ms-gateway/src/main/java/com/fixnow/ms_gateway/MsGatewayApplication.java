eb9c0d24f3a912b22e38fcd52f2e4ae979e1e69b
Jesus David Amaya Pastran <jesu.amaya@duocuc.cl>
2026-05-24 00:56:28 -0400

    fix: se agregan en maven los pom.xml de todos los ms

--- ms-gateway/src/main/java/com/fixnow/ms_gateway/MsGatewayApplication.java
+++ ms-gateway/src/main/java/com/fixnow/ms_gateway/MsGatewayApplication.java
@@ -1,6 +1,6 @@
 package com.fixnow.ms_gateway;
 
-import -org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
